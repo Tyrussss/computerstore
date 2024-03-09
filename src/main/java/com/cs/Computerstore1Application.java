319a3b8ed72e88faa6cc518e79c5fba57d39eb5c
Tuyen 'Tyrus' Ng <trgtuyen@gmail.com>
2024-03-09 20:54:53 +0700

    re-up
    Maven 17

---
+++ src/main/java/com/cs/Computerstore1Application.java
@@ -0,0 +1,13 @@
+package com.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Computerstore1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Computerstore1Application.class, args);
+	}
+
+}
