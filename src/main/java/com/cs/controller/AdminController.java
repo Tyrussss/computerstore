5bfe18e63ff9ec07dc5afdc53f04ed443bf60f38
Tuyen 'Tyrus' Ng <trgtuyen@gmail.com>
2024-03-29 21:39:48 +0700

    account
    aa

--- src/main/java/com/cs/controller/AdminController.java
+++ src/main/java/com/cs/controller/AdminController.java
@@ -1,9 +1,7 @@
 package com.cs.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +13,8 @@ import com.cs.repository.UserRepository;
 @RequestMapping("/admin")
 
 public class AdminController {
-		
-	UserRepository userRep = new UserRepository();
+	@Autowired
+	UserRepository userRep;
 	
 	@GetMapping("")
 	public String adindex() {
