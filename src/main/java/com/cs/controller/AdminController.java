6f40f323864c4bf255918b61d78d0484a9457d30
Tuyen 'Tyrus' Ng <trgtuyen@gmail.com>
2024-04-02 20:04:28 +0700

    a

--- src/main/java/com/cs/controller/AdminController.java
+++ src/main/java/com/cs/controller/AdminController.java
@@ -1,16 +1,22 @@
 package com.cs.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.cs.model.User;
+import com.cs.model.UserReg;
 import com.cs.repository.UserRepository;
 
 @Controller
-@RequestMapping("")
+@RequestMapping("/admin")
 
 public class AdminController {
 	@Autowired
@@ -25,8 +31,31 @@ public class AdminController {
 	public String account(Model model) {
 		
 		model.addAttribute("user", userRep.findAll());
+		model.addAttribute("userReg", new UserReg());
+		
+		List<String> listRole = Arrays.asList("Admin", "Customer", "Subcriber");
+        model.addAttribute("listRole", listRole);
 		return "admin/account";
 	}
+	@GetMapping("/register")
+    public String showForm(Model model) {
+        
+        model.addAttribute("user", new User());
+         
+        List<String> listRole = Arrays.asList("Admin", "Customer", "Subcriber");
+        model.addAttribute("listRole", listRole);
+		return "admin/account";
+    }
+	
+	@PostMapping("/register/save")
+    public String saveForm(Model model, User user) {
+        
+        model.addAttribute("user", userRep.insert(user));
+         
+        List<String> listRole = Arrays.asList("Admin", "Customer", "Subcriber");
+        model.addAttribute("listRole", listRole);
+		return "admin/register";
+    }
 	
 	
 }
