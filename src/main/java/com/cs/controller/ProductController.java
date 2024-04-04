ac255f56c8715f4db0e076df5665ff99b8b4ec7c
arthurvu <thuvta0207@gmail.com>
2024-04-04 18:44:38 +0700

    Update ProductController.java

--- src/main/java/com/cs/controller/ProductController.java
+++ src/main/java/com/cs/controller/ProductController.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cs.model.Product;
 import com.cs.repository.BrandRepository;
 import com.cs.repository.CategoryRepository;
 import com.cs.repository.ProductRepository;
+
 
 @Controller
 @RequestMapping("")
@@ -26,4 +30,15 @@ public class ProductController {
 		model.addAttribute("brand", brcep.findAll());
 		return "admin/product";
 	}
+	@GetMapping("/product/new")
+    public String newUserForm(Model model) {
+        model.addAttribute("user", new Product());
+        return "newProduct";
+    }
+
+    @PostMapping("/product")
+    public String createUser(@ModelAttribute Product product) {
+        rep.insert(product);
+        return "redirect:/";
+    }
 }
